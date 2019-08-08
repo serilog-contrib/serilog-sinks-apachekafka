dc6f6a5e1e16d1c6e0ed390d4d2b9108f352d0a4
Vadim Hatsura <vadimgatsura@gmail.com>
2019-08-08 07:49:03 +0300

    Tests enhancements (#2)

---
+++ src/Serilog.Sinks.Kafka/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Serilog.Sinks.Kafka.Tests")]
---
+++ tests/Serilog.Sinks.Kafka.Tests/Sinks/ModeSwitcherTests.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using Confluent.Kafka;
+using Serilog.Sinks.Kafka.Sinks;
+using Xunit;
+
+namespace Serilog.Sinks.Kafka.Tests.Sinks
+{
+    [ExcludeFromCodeCoverage]
+    public class ModeSwitcherTests
+    {
+        [Fact]
+        public void Constructor_ShouldThrowsException_WhenFallbackTimeIsLessOrEqualZero()
+        {
+            // Arrange + Act + Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ModeSwitcher(TimeSpan.Zero));
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new ModeSwitcher(TimeSpan.Zero.Subtract(TimeSpan.FromMinutes(1))));
+        }
+
+        public static IEnumerable<object[]> CurrentModeTestData
+        {
+            get
+            {
+                yield return new object[]
+                    {new Action<ModeSwitcher>(switcher => switcher.SwitchToFailover(new Exception()))};
+                yield return new object[]
+                {
+                    new Action<ModeSwitcher>(switcher =>
+                        switcher.SwitchToFailover(new Error(ErrorCode.NetworkException)))
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(CurrentModeTestData))]
+        internal void CurrentMode_ShouldSwitchModeToPrimary_AfterFallbackTime(Action<ModeSwitcher> action)
+        {
+            // Arrange
+            var fallbackTime = TimeSpan.FromSeconds(5);
+
+            var switcher = new ModeSwitcher(fallbackTime);
+
+            // Act
+            action.Invoke(switcher);
+
+            // Assert
+            Assert.Equal(Mode.Failover, switcher.CurrentMode);
+            Thread.Sleep(fallbackTime.Add(TimeSpan.FromSeconds(1)));
+            Assert.Equal(Mode.Primary, switcher.CurrentMode);
+        }
+    }
+}
